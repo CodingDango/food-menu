786dc58a873eda655bfe2ea8388a7ad671a77084
CodingDango <babyyodaman2.0@gmail.com>
2026-01-11 22:02:21 +0800

    before component refactor

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const products = [
 export default function Menu() {
   return (
     <div className="p-2xl grid place-items-center">
-      <div className="max-w-6xl w-full">
+      <div className="max-w-7xl w-full">
         <div className="flex flex-col gap-2xl">
           <h1 className="font-medium text-4xl leading-normal">
             <span className="text-primary">Menu</span> That Always <br /> Will
@@ -72,50 +72,101 @@ export default function Menu() {
             </div>
 
             <div className="flex flex-col gap-lg">
-              <div className="bg-card grid grid-cols-[1fr_300px] p-lg rounded-2xl">
-                <div className="flex flex-col gap-md">
+              <div className="bg-card grid grid-cols-[1fr_300px] p-lg rounded-2xl gap-sm  ">
+                <div className="space-y-md">
                   <h2 className="text-3xl font-medium ">Veggie Burger</h2>
                   <p className="text-muted-foreground">
                     A delectable patty filled with potatoes, peas, carrots and
                     tasty Indian spices. Topped with crispy lettuce and mayo.
                   </p>
-                  <div className="flex bg-stone-50 rounded-2xl overflow-hidden justify-between">
-                    <div className="px-lg py-md flex flex-col gap-xs border-r">
-                      <div className="flex flex-col items-center">
-                        <FlameIcon/>
-                        <span className="font-medium">Energy</span>
+                  <div className="bg-secondary rounded-2xl overflow-hidden justify-between inline-block">
+                    <div className="flex">
+                      <div className="py-md flex flex-col gap-xs">
+                        <div className="px-lg">
+                          <div className="flex flex-col items-center">
+                            <FlameIcon />
+                            <span className="font-medium">Energy</span>
+                          </div>
+                          <span className="text-xl font-medium text-primary">
+                            334 kCal
+                          </span>
+                        </div>
                       </div>
-                      <span className="text-xl font-medium text-primary">334 kCal</span>
-                    </div>
-                    <div className="px-lg py-md flex flex-col gap-xs border-r">
-                      <div className="flex flex-col items-center">
-                        <FlameIcon/>
-                        <span className="font-medium">Energy</span>
+                      <div className="py-md flex flex-col gap-xs">
+                        <div className="px-lg">
+                          <div className="flex flex-col items-center">
+                            <FlameIcon />
+                            <span className="font-medium">Energy</span>
+                          </div>
+                          <span className="text-xl font-medium text-primary">
+                            334 kCal
+                          </span>
+                        </div>
                       </div>
-                      <span className="text-xl font-medium text-primary">334 kCal</span>
-                    </div>
-                    <div className="px-lg py-md flex flex-col gap-xs">
-                      <div className="flex flex-col items-center">
-                        <FlameIcon/>
-                        <span className="font-medium">Energy</span>
+                      <div className="py-md flex flex-col gap-xs">
+                        <div className="px-lg">
+                          <div className="flex flex-col items-center">
+                            <FlameIcon />
+                            <span className="font-medium">Energy</span>
+                          </div>
+                          <span className="text-xl font-medium text-primary">
+                            334 kCal
+                          </span>
+                        </div>
                       </div>
-                      <span className="text-xl font-medium text-primary">334 kCal</span>
                     </div>
                   </div>
-                  
                 </div>
 
-                <div className="flex flex-col gap-sm">
+                <div className="flex flex-col gap-xs">
                   <div className="w-full md:w-[300px] h-[250px] relative flex-shrink-0">
                     <img
                       src="/burger.png"
                       alt="Burger"
-                      className="w-full h-full object-contain drop-shadow-xl"
+                      className="w-full h-full object-contain drop-shadow-xl scale-125"
                     />
+                    <span className="rounded-full px-md p-1 absolute bottom-0 left-1/2 -translate-x-1/2 text-3xl text-primary-foreground bg-primary font-medium self-center">
+                      5.99 $
+                    </span>
                   </div>
-                  <span className="text-4xl text-primary font-medium self-center">5.99 $</span>
                 </div>
-                <div></div>
+              </div>
+
+              <div className="grid grid-cols-3 gap-lg flex-wrap">
+                <div className="bg-card p-lg rounded-2xl flex flex-col items-center gap-lg">
+                  
+                    <div className="w-[150px] h-[100px]">
+                      <img src="/burger.png" className="object-contain drop-shadow-2xl" />
+                    </div>
+
+                    <p className="font-medium text-lg text-center">Chicken Burger</p>
+                </div>
+
+                <div className="bg-card p-lg rounded-2xl flex flex-col gap-lg items-center">
+                  
+                    <div className="w-[150px] h-[100px]">
+                      <img src="/burger.png" className="object-contain drop-shadow-2xl" />
+                    </div>
+
+                    <p className="font-medium text-lg text-center">Chicken Burger</p>
+                </div>
+                <div className="bg-card p-lg rounded-2xl flex flex-col gap-lg items-center">
+                  
+                    <div className="w-[150px] h-[100px]">
+                      <img src="/burger.png" className="object-contain drop-shadow-2xl" />
+                    </div>
+
+                    <p className="font-medium text-lg text-center">Chicken Burger</p>
+                </div>
+                <div className="bg-card p-lg rounded-2xl flex flex-col gap-lg items-center">
+                  
+                    <div className="w-[150px] h-[100px]">
+                      <img src="/burger.png" className="object-contain drop-shadow-2xl" />
+                    </div>
+
+                    <p className="font-medium text-lg text-center">Chicken Burger</p>
+                </div>
+
               </div>
             </div>
 
